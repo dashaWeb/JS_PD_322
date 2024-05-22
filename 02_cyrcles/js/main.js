73e4e78b9dc7a211fc0f1afbd619b1ade86a1281
konopelko_d <konopelko_d@itstep.academy>
2024-05-22 16:53:56 +0300

    cyrcles

---
+++ 02_cyrcles/js/main.js
@@ -0,0 +1,42 @@
+
+
+// let i = 0;
+// while(i < 10)
+// {
+//     //document.write("<p> " + i + " </p>")
+//     i++;
+//     document.write(`<p style = "text-align:center;"> ${i} </p>`)
+// }
+
+// let res;
+// do{
+//    res = prompt("2 + 2 = ?");
+// }while(res != 4)
+
+// alert("Good!!!")
+
+// for (let i = 1; i <=10; i++) {
+//     document.write(`<p style = "text-align:center;"> ${i} </p>`)  
+// }
+// let i = 1;
+
+// for (;;) {
+//     document.write(`<p style = "text-align:center;"> ${i} </p>`)  
+//     i++;
+// }
+
+
+// document.write("<ul>")
+// while(true)
+// {
+//     let number = prompt("Enter number");
+//     if(number == null)
+//     {
+//         break;
+//     }
+//     if(isNaN(parseFloat(number))){
+//         continue;
+//     }
+//     document.write(`<li> ${number} </li>`)
+// }
+// document.write("</ul>")
---
+++ 03_function/js/main.js
@@ -0,0 +1,60 @@
+// console.log(sum(2,5))
+
+// function sum(a,b = 2)
+// {
+//    return a + b;
+// }
+
+// let res = sum(2);
+// console.log(res)
+
+
+// let getSeconds = function(minutes)
+// {
+//     return minutes * 60;
+// }
+// console.log(getSeconds(20))
+
+
+// let getSeconds = (minutes) => {
+//     return minutes * 60;
+// }
+
+// let getSeconds = minutes => {
+//     return minutes * 60;
+// }
+
+//let getSeconds = (m,h) => m * 60 + h * 3600;
+//console.log(getSeconds(32,2));
+
+
+// let sum = (... args) => {
+//     let sum = 0;
+//     for (let i = 0; i < args.length; i++) {
+//         sum += args[i];
+//     }
+//     return sum;
+// }
+// console.log(sum(1,2,3,4,5,6));
+
+
+// 5! = 5 * 4!
+// 4! = 4 * 3!
+// 3! = 3 * 2!
+// 2! = 2 * 1!
+// 1! = 1
+// 0! = 1
+
+
+// function factorial(number)
+// {
+//     if(number == 1 || number == 0) return 1;
+//     return number * factorial(number - 1);
+// }
+
+// console.log(factorial(5))
+
+
+// (function(){
+//     document.write("Hello anonim function")
+// }())
